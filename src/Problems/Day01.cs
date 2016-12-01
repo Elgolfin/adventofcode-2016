2095dd5e9947355b8ff824ef7fd2ebbfb74a0485
Elgolfin <elgo@nicomputer.net>
2016-12-01 10:28:08 -0500

    Solve Day 1 Puzzle, Part I

--- src/Problems/Day01.cs
+++ src/Problems/Day01.cs
@@ -1,16 +1,114 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode1016
 {
     public class Day01
     {
-        public Day01()
-        {    
+        
+        public int x = 0;
+        public int y = 0;
+        public enum Direction {North, East, South, West}; 
+        public Direction HeadTo = Direction.North;
+
+        public List<string> Path = new List<string>();
+        
+        public Day01(string path)
+        {
+            Path = ParsePath(path);
         }
 
-        public static string Test()
-        {
-            return "Hiya Foo!";
+        private List<string> ParsePath (string path) {
+            var route = path.Split(',').Select(p => p.Trim()).ToList();
+            return route;
+        }
+
+        public void MoveAlongThePath () {
+            foreach (var code in Path) {
+                MoveTo(code);
+            }
+        }
+
+        public int GetBlocksAwayFromStart () {
+            return Math.Abs(x) + Math.Abs(y);
+        }
+
+        public void MoveTo (string code) {
+            
+            var dir = String.Empty;
+            var dist = 0;
+
+            string pattern = @"(?<dir>[RL])(?<dist>[0-9]+)$";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(code);
+            if (match.Success)
+            {
+                dir = match.Groups["dir"].Value;
+                Int32.TryParse(match.Groups["dist"].Value, out dist);
+            }
+
+            switch (HeadTo) {
+                case Direction.North:
+                    GoFromNorthTo(dir, dist);
+                    break;
+                case Direction.South:
+                    GoFromSouthTo(dir, dist);
+                    break;
+                case Direction.East:
+                    GoFromEastTo(dir, dist);
+                    break;
+                case Direction.West:
+                    GoFromWestTo(dir, dist);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void GoFromNorthTo (string dir, int dist) {
+            if (dir == "R") {
+                x += dist;
+                HeadTo = Direction.East;
+            }
+            if (dir == "L") {
+                x -= dist;
+                HeadTo = Direction.West;
+            }
+        }
+
+        public void GoFromSouthTo (string dir, int dist) {
+            if (dir == "R") {
+                x -= dist;
+                HeadTo = Direction.West;
+            }
+            if (dir == "L") {
+                x += dist;
+                HeadTo = Direction.East;
+            }
+        }
+
+        public void GoFromEastTo (string dir, int dist) {
+            if (dir == "R") {
+                y -= dist;
+                HeadTo = Direction.South;
+            }
+            if (dir == "L") {
+                y += dist;
+                HeadTo = Direction.North;
+            }
+        }
+
+        public void GoFromWestTo (string dir, int dist) {
+            if (dir == "R") {
+                y += dist;
+                HeadTo = Direction.North;
+            }
+            if (dir == "L") {
+                y -= dist;
+                HeadTo = Direction.South;
+            }
         }
     }
 }
--- test/Day01UnitTests.cs
+++ test/Day01UnitTests.cs
@@ -10,13 +10,134 @@ namespace AdventOfCode1016Tests
     public class Day01UnitTests
     {
         [Fact]
-        public void Test_Should_Return_Hiya_Foo() 
+        public void ParseString_R2_L2_R3_Should_Return_A_List_Of_3_Elements() 
         {
-            var p = new Day01();
-            Assert.Equal("Hiya Foo!", Day01.Test());
+            var input = "R2, R2, L3";
+            var d = new Day01(input);
+            var expected = new List<String>{"R2", "R2", "L3"};
+            Assert.Equal(expected, d.Path);
         }
 
-        
+        [Fact]
+        public void Facing_North_R2_Should_Head_To_East_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.MoveTo("R2");
+            Assert.Equal(Day01.Direction.East, d.HeadTo);
+            Assert.Equal(2, d.x);
+            Assert.Equal(0, d.y);
+        }
+
+        [Fact]
+        public void Facing_North_L2_Should_Head_To_West_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.MoveTo("L2");
+            Assert.Equal(Day01.Direction.West, d.HeadTo);
+            Assert.Equal(-2, d.x);
+            Assert.Equal(0, d.y);
+        }
+
+        [Fact]
+        public void Facing_South_R2_Should_Head_To_West_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.South;
+            d.MoveTo("R2");
+            Assert.Equal(Day01.Direction.West, d.HeadTo);
+            Assert.Equal(-2, d.x);
+            Assert.Equal(0, d.y);
+        }
+
+        [Fact]
+        public void Facing_South_L2_Should_Head_To_East_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.South;
+            d.MoveTo("L2");
+            Assert.Equal(Day01.Direction.East, d.HeadTo);
+            Assert.Equal(2, d.x);
+            Assert.Equal(0, d.y);
+        }
+
+        [Fact]
+        public void Facing_East_R2_Should_Head_To_South_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.East;
+            d.MoveTo("R2");
+            Assert.Equal(Day01.Direction.South, d.HeadTo);
+            Assert.Equal(-2, d.y);
+            Assert.Equal(0, d.x);
+        }
+
+        [Fact]
+        public void Facing_East_L2_Should_Head_To_North_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.East;
+            d.MoveTo("L2");
+            Assert.Equal(Day01.Direction.North, d.HeadTo);
+            Assert.Equal(2, d.y);
+            Assert.Equal(0, d.x);
+        }
+
+        [Fact]
+        public void Facing_West_R2_Should_Head_To_North_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.West;
+            d.MoveTo("R2");
+            Assert.Equal(Day01.Direction.North, d.HeadTo);
+            Assert.Equal(2, d.y);
+            Assert.Equal(0, d.x);
+        }
+
+        [Fact]
+        public void Facing_West_L2_Should_Head_To_South_2_Blocks() 
+        {
+            var input = "";
+            var d = new Day01(input);
+            d.HeadTo = Day01.Direction.West;
+            d.MoveTo("L2");
+            Assert.Equal(Day01.Direction.South, d.HeadTo);
+            Assert.Equal(-2, d.y);
+            Assert.Equal(0, d.x);
+        }
+
+        [Fact]
+        public void Moving_R2_L3_Should_Return_5_Blocks_Away() 
+        {
+            var input = "R2, L3";
+            var d = new Day01(input);
+            d.MoveAlongThePath();
+            Assert.Equal(5, d.GetBlocksAwayFromStart());
+        }
+
+        [Fact]
+        public void Moving_R2_R2_R2_Should_Return_2_Blocks_Away() 
+        {
+            var input = "R2, R2, R2";
+            var d = new Day01(input);
+            d.MoveAlongThePath();
+            Assert.Equal(2, d.GetBlocksAwayFromStart());
+        }
+
+        [Fact]
+        public void Moving_R5_L5_R5_R3_Should_Return_12_Blocks_Away() 
+        {
+            var input = "R5, L5, R5, R3";
+            var d = new Day01(input);
+            d.MoveAlongThePath();
+            Assert.Equal(12, d.GetBlocksAwayFromStart());
+        }
         
     }
 }
