f0ae54f2bcc11b417a5a2a2d7add7b27fbc1f456
Elgolfin <elgo@nicomputer.net>
2016-12-03 08:23:17 -0500

    Solve Day 3 Puzzle, Part I

---
+++ src/AdventOfCode2016/Day03.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode1016
+{
+    public class Day03
+    {
+
+        public List<string> triangles = new List<string>();
+        public int NumberOfPossibleTriangles = 0;
+
+        public Day03(string input)
+        {
+            triangles = input.Split('\n').Select(p => p.Trim()).ToList();
+        }
+
+        public void ParseTriangles () 
+        {
+            foreach (var triangle in triangles) {
+                if (IsTriangle(triangle)) {
+                    NumberOfPossibleTriangles++;
+                }
+            }
+        }
+
+        private bool IsTriangle (string triangle) 
+        {
+            int side1;
+            int side2;
+            int side3;
+
+            var sides = triangle.Split(new string[] { "  " }, StringSplitOptions.None).Select(p => p.Trim()).ToList();
+            Int32.TryParse(sides[0], out side1);
+            Int32.TryParse(sides[1], out side2);
+            Int32.TryParse(sides[2], out side3);
+            
+            if (side1 + side2 <= side3) { return false; }
+            if (side1 + side3 <= side2) { return false; }
+            if (side2 + side3 <= side1) { return false; }
+            
+            return true;
+        }
+    }
+}
---
+++ test/Day03UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using AdventOfCode1016;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode1016Tests
+{
+    public class Day03UnitTests
+    {
+        
+        [Fact]
+        public void Triangle_With_Sides_5_10_25_Is_Not_A_Triangle() 
+        {
+            var input = "5  10  25";
+            var d = new Day03(input);
+            d.ParseTriangles();
+            Assert.Equal(0, d.NumberOfPossibleTriangles);
+        }
+
+        [Fact]
+        public void Triangle_With_Sides_5_5_5_Is_A_Triangle() 
+        {
+            var input = "5  5  5";
+            var d = new Day03(input);
+            d.ParseTriangles();
+            Assert.Equal(1, d.NumberOfPossibleTriangles);
+        }
+
+        [Fact]
+        public void Triangle_With_Sides_775_785_361_And_Whitespaces_In_Input_Is_A_Triangle() 
+        {
+            var input = "  775  785  361";
+            var d = new Day03(input);
+            d.ParseTriangles();
+            Assert.Equal(1, d.NumberOfPossibleTriangles);
+        }
+
+        [Fact]
+        public void No_Triangles_From_Input_Should_Return_0() 
+        {
+            var input = "5  10  25\n5  10  25";
+            var d = new Day03(input);
+            d.ParseTriangles();
+            Assert.Equal(0, d.NumberOfPossibleTriangles);
+        }
+
+        [Fact]
+        public void Triangles_From_Input_Should_Return_2() 
+        {
+            var input = "5  5  5\n5  5  5";
+            var d = new Day03(input);
+            d.ParseTriangles();
+            Assert.Equal(2, d.NumberOfPossibleTriangles);
+        }
+
+    }
+}
