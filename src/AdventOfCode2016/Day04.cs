a25af0fc6c88b0c2492f0573925cc5fb27550954
Elgolfin <elgo@nicomputer.net>
2016-12-04 08:50:29 -0500

    Solve Day 4 puzzle, Part I

---
+++ src/AdventOfCode2016/Day04.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode1016
+{
+    public class Day04
+    {
+        public string EncryptedNames;
+        public int SectorIdSum = 0;
+
+        public Day04(string input)
+        {
+            EncryptedNames = input;
+            ParseEncryptedNames();
+        }
+
+        private void ParseEncryptedNames () {
+            var lines = EncryptedNames.Split('\n').Select(p => p.Trim()).ToList();
+            foreach (var encryptedName in lines) {
+                Room r = new Room(encryptedName);
+                if (r.IsRealRoom()) {
+                    SectorIdSum += r.SectorId;
+                }
+            }
+        }
+    }
+
+    public class Room {
+
+        public string EncryptedData = String.Empty;
+        public string EncryptedName = String.Empty;
+        public int SectorId;
+        public string Checksum = String.Empty;
+
+        public Room (string encryptedData){
+            EncryptedData = encryptedData;
+            DecryptData();
+        }
+
+        private void DecryptData () {
+            // aaaaa-bbb-z-y-x-123[abxyz]
+            string pattern = @"^(?<encryptedname>[-a-z]+)(?<sectorid>[0-9]+)\[(?<checksum>[a-z]{5})\]$";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(EncryptedData);
+            if (match.Success)
+            {
+                EncryptedName = match.Groups["encryptedname"].Value;
+                // Remove the last dash
+                EncryptedName = EncryptedName.Substring(0, EncryptedName.Length - 1);
+                Checksum = match.Groups["checksum"].Value;
+                Int32.TryParse(match.Groups["sectorid"].Value, out SectorId);
+            }
+        }
+
+        public string CalculateChecksum () {
+            string checksum = String.Empty;
+            SortedDictionary<char, int> letters = new SortedDictionary<char, int>();
+            foreach (var c in EncryptedName) {
+                if (Char.IsLetter(c)) {
+                    if (letters.Keys.Contains(c)) {
+                        letters[c]++;
+                    } else {
+                        letters.Add(c, 1);
+                    }
+                }
+            }
+            var top5 = letters.OrderByDescending(pair => pair.Value).Take(5);
+            foreach (var letter in top5) {
+                checksum += letter.Key;
+            }
+            return checksum;
+        }
+
+        public bool IsRealRoom () {
+            return CalculateChecksum() == Checksum;
+        }
+
+    }
+}
---
+++ test/Day04UnitTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Xunit;
+using AdventOfCode1016;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode1016Tests
+{
+    public class Day04UnitTests
+    {
+        
+        [Fact]
+        public void Should_Instanciate_Room_Properly_1() 
+        {
+            var input = "aaaaa-bbb-z-y-x-123[abxyz]";
+            var room = new Room(input);
+            Assert.Equal("aaaaa-bbb-z-y-x", room.EncryptedName);
+            Assert.Equal(123, room.SectorId);
+            Assert.Equal("abxyz", room.Checksum);
+        }
+
+        [Fact]
+        public void Room_1_Should_Be_A_Real_Room() 
+        {
+            var input = "aaaaa-bbb-z-y-x-123[abxyz]";
+            var room = new Room(input);
+            Assert.Equal(true, room.IsRealRoom());
+        }
+
+        [Fact]
+        public void Room_1_Checksum_Should_Be_abxyz() 
+        {
+            var input = "aaaaa-bbb-z-y-x-123[abxyz]";
+            var room = new Room(input);
+            Assert.Equal("abxyz", room.CalculateChecksum());
+        }
+
+        [Fact]
+        public void Should_Instanciate_Room_Properly_2() 
+        {
+            var input = "a-b-c-d-e-f-g-h-987[abcde]";
+            var room = new Room(input);
+            Assert.Equal("a-b-c-d-e-f-g-h", room.EncryptedName);
+            Assert.Equal(987, room.SectorId);
+            Assert.Equal("abcde", room.Checksum);
+        }
+
+        [Fact]
+        public void Room_2_Checksum_Should_Be_abcde() 
+        {
+            var input = "a-b-c-d-e-f-g-h-987[abcde]";
+            var room = new Room(input);
+            Assert.Equal("abcde", room.CalculateChecksum());
+        }
+
+        [Fact]
+        public void Room_2_Bis_Checksum_Should_Be_bacde() 
+        {
+            var input = "b-b-c-d-e-f-g-a-987[bacde]";
+            var room = new Room(input);
+            Assert.Equal("bacde", room.CalculateChecksum());
+        }
+
+        [Fact]
+        public void Room_2_Should_Be_A_Real_Room() 
+        {
+            var input = "a-b-c-d-e-f-g-h-987[abcde]";
+            var room = new Room(input);
+            Assert.Equal(true, room.IsRealRoom());
+        }
+
+        [Fact]
+        public void Should_Instanciate_Room_Properly_3() 
+        {
+            var input = "not-a-real-room-404[oarel]";
+            var room = new Room(input);
+            Assert.Equal("not-a-real-room", room.EncryptedName);
+            Assert.Equal(404, room.SectorId);
+            Assert.Equal("oarel", room.Checksum);
+        }
+
+        [Fact]
+        public void Room_3_Should_Be_A_Real_Room() 
+        {
+            var input = "not-a-real-room-404[oarel]";
+            var room = new Room(input);
+            Assert.Equal(true, room.IsRealRoom());
+        }
+
+        [Fact]
+        public void Should_Instanciate_Room_Properly_4() 
+        {
+            var input = "totally-real-room-200[decoy]";
+            var room = new Room(input);
+            Assert.Equal("totally-real-room", room.EncryptedName);
+            Assert.Equal(200, room.SectorId);
+            Assert.Equal("decoy", room.Checksum);
+        }
+
+        [Fact]
+        public void Room_4_Should_Not_Be_A_Real_Room() 
+        {
+            var input = "totally-real-room-200[decoy]";
+            var room = new Room(input);
+            Assert.Equal(false, room.IsRealRoom());
+        }
+
+        [Fact]
+        public void Sum_Of_Rooms_SectorId_Should_Return_1514() 
+        {
+            var input = @"aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]";
+            var d04 = new Day04(input);
+            Assert.Equal(1514, d04.SectorIdSum);
+        }
+        
+
+    }
+}
